67f80b94cd46c358185d9b6616a31b5ff63cce51
LudiusMaximus <4678285+LudiusMaximus@users.noreply.github.com>
2023-12-30 00:09:15 +0100

    Bug fix for dragonriding situation scripts.

--- DefaultSettings.lua
+++ DefaultSettings.lua
@@ -330,10 +330,10 @@ return false]],
         events = {"PLAYER_MOUNT_DISPLAY_CHANGED"},
         executeOnInit = [[this.lastActiveMount = nil
 
-this.GetCurrentMount = function()
+this.IsCurrentMountForDragonriding = function()
   if this.lastActiveMount then
     local _, _, _, isActive, _, _, _, _, _, _, _, _, isForDragonriding = C_MountJournal.GetMountInfoByID(this.lastActiveMount)
-    if active then
+    if isActive then
       return isForDragonRiding
     end
   end
@@ -349,17 +349,17 @@ this.GetCurrentMount = function()
   return nil
 end]],
         priority = 101,
-        condition = [[return IsMounted() and this.GetCurrentMount()]],
+        condition = [[return IsMounted() and this.IsCurrentMountForDragonriding()]],
       },
       ["126"] = {
         name = "Dragonriding (flying)",
         events = {"PLAYER_MOUNT_DISPLAY_CHANGED", "UNIT_AURA"},
         executeOnInit = [[this.lastActiveMount = nil
 
-this.GetCurrentMount = function()
+this.IsCurrentMountForDragonriding = function()
   if this.lastActiveMount then
     local _, _, _, isActive, _, _, _, _, _, _, _, _, isForDragonriding = C_MountJournal.GetMountInfoByID(this.lastActiveMount)
-    if active then
+    if isActive then
       return isForDragonRiding
     end
   end
@@ -375,7 +375,7 @@ this.GetCurrentMount = function()
   return nil
 end]],
         priority = 102,
-        condition = [[return IsMounted() and IsFlying() and this.GetCurrentMount()]],
+        condition = [[return IsMounted() and IsFlying() and this.IsCurrentMountForDragonriding()]],
       },
       ["200"] = {
         name = "Hearth/Teleport",
